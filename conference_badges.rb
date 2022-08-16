96392dbc7a77f49cf958eb50db032a5a8290c4d2
Jackline Jebet <jebetjackline2016@gmail.com>
2022-08-16 10:47:34 +0300

    commit tree dd641ecec55ad1e10b5d2ba83455a2733403b829

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+
+def batch_badge_creator(attendees)
+    array = []
+    attendees.each {|name| array << badge_maker(name)}
+    return array    
+end
+
+
+
+def assign_rooms(attendees)
+    new_array =[]
+    attendees.each_with_index do |name,index|
+        new_array<<"Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+    return new_array
+end
+
+
+def printer(attendees)
+    batch_badge_creator(attendees).each{|names|puts names}
+    assign_room(attendees).each{|room|puts room}
+end
+
